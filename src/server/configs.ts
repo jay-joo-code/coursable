f554d388da2d4b637d157cfb80d11e74afa92e8a
Jay Joo <jj534@cornell.edu>
2021-03-13 10:26:11 +0900

    Init 2

--- src/server/configs.ts
+++
@@ -1,5 +0,0 @@
-import { IConfigs } from "./domain/IConfigs";
-
-export const configs: IConfigs = {
-    mongoUrl: 'mongodb+srv://admin:nkyiTm7PmJdORthl@boilerplate-cluster.1atkx.mongodb.net/boilerplateDatabase?retryWrites=true&w=majority'
-}
--- src/server/domain/IConfigs.ts
+++
@@ -1,3 +0,0 @@
-export interface IConfigs {
-    mongoUrl: string
-}
